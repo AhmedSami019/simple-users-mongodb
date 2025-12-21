cc1826c4c20463b083d9c4352e6d9d0632c64c97
ahmedsami019 <easami019192@gmail.com>
2025-12-21 13:03:17 +0600

    added the mongodb into server

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-const express = require('express');
-const app = express()
-const port = process.env.PORT || 3002
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3002;
 
-app.get('/', (req, res)=>{
-    res.send("user is coming")
-})
+// user : simpleDbUser2
+// pass : hfeUZl0jttd1tKeR
 
-app.listen(port, ()=>{
-    console.log(`data is coming form port : ${port}`)
-})
+const uri =
+  "mongodb+srv://simpleDbUser2:hfeUZl0jttd1tKeR@maincluster0.m4dyknx.mongodb.net/?appName=MainCluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+// function
+const run = async () => {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+
+  }
+};
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.send("user is coming");
+});
+
+app.listen(port, () => {
+  console.log(`data is coming form port : ${port}`);
+});
